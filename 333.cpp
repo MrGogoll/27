4d10a000dd4511926f7c6bbdaff17fb67720cc4b
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-11-20 15:32:26 +0300

    Add files via upload

---
+++ 333.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main()
+{
+	string s;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (s[0] == '1' || s[i] == '3' || s[i] == '5' || s[i] == '7' || s[i] == '9') {
+			s[i] = ' ';
+		}
+		if (s[i]=='1'|| s[i] == '3' || s[i] == '5'|| s[i] == '7'|| s[i] == '9') {
+			s[i] = '0';
+		
+		}
+	}
+	cout << s;
+
+
+
+}
+
